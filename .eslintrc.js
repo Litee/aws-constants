97a84e47078b3c0d7501d62d02244870ce4b968d
Litee <lipatkin@gmail.com>
2021-03-19 12:08:36 +0000

    Linted files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
-    "plugins": ["@typescript-eslint", "prettier"],
-}
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint", "prettier"],
+};
