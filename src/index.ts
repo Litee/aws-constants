205e0cb3b991b3ead443e67fcf73a082e5a3715c
Litee <lipatkin@gmail.com>
2021-03-19 13:17:09 +0000

    Added AWS service principals

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./aws-api-gateway";
 export * from "./aws-cloudwatch";
 export * from "./aws-dynamodb";
+export * from "./aws-iam";
 export * from "./aws-lambda";
 export * from "./aws-step-functions";
 export * from "./regions";
